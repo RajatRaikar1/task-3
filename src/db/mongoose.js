d04c2cb4762d8265edc202d749e8efe467d93cc3
RajatRaikar1 <88779767+RajatRaikar1@users.noreply.github.com>
2021-09-12 20:05:24 +0530

    Create mongoose.js

---
+++ src/db/mongoose.js
@@ -0,0 +1,10 @@
+const mongosse = require('mongoose')
+
+mongosse.connect('mongodb://127.0.0.1:27017/management-system', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() =>{
+    console.log('connected')
+}) 
